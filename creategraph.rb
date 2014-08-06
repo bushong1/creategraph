ce40e62a2336855080fe6397336779162a6d8520
Charles Bushong <cbushong@fearsol.com>
2014-08-06 12:17:16 -0400

    Adding option for 2 lines, adding margin settings

--- creategraph.rb
+++ creategraph.rb
@@ -1,23 +1,33 @@
 #!/usr/bin/env ruby
 require 'graph'
 
+def truncate(the_string)
+  #Set the maximum amount of characters to appear in a node
+  node_character_limit = 30
+  return the_string unless the_string.length > node_character_limit
+  return the_string[0..node_character_limit] + "..."
+end
+
 unless ARGV[0]
   puts "usage: ruby #{$0} <data file, pipe separated>"
   exit -1
 end
+
 nodenames = {}
 
-node_character_limit = 30
+
+#Set the margin of a node.  Default is "1", whatever that means.
+margin = "0.05"
+
 weight = 0
 digraph do
   
   # Set the font name and size.  Details here:  http://www.graphviz.org/doc/fontfaq.txt
-  the_font = font "Times"
-  #the_font = font "Helvetica"
+  the_font = font "Times-Roman"
   the_fontsize = fontsize "10"
   # set box color
   the_color = color "blueviolet"
-  node_attribs << the_fontsize << the_font << the_color
+  node_attribs << the_fontsize << the_font << the_color << "margin=#{margin}" << "height=#{margin}"
 
   # Orientation.  Defaults "TB" (top to bottom), alternate "LR" (left to right)
   orient "LR"
@@ -34,22 +44,15 @@ digraph do
     base = base.to_s
 
     # Contents of each node
-    nodenames[current] = "" + current+" "+line[1].strip
+    nodenames[current] = truncate("#{current} #{line[1].strip}") 
+    ## For two lines...
+    # nodenames[current] = truncate("#{current} #{line[1].strip}") + "\n" + truncate("#{line[2].strip}")
 
-    # Truncate
-    if nodenames[current].length > node_character_limit
-      #To only do for level 3+, count the dots like this:  if current.count(".") >= 2
-      nodenames[current] = nodenames[current][0..node_character_limit] + "..."
-    end
     if current != base
       curr_edge = edge nodenames[base], nodenames[current]
-      #Tried messing with line weight (aka, length) but it isn't working
-      #if (current.count(".") == 2) 
-      #  weight = weight +1;
-      #  curr_edge.attributes << "weight=#{(weight % 3) * 5+5}."
-      #end
     end
   end
 
   save ARGV[0], 'png'
 end
+
