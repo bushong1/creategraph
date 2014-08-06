f57f775769536150c60730b0b63ec2daf24f0b5f
Charles Bushong <cbushong@fearsol.com>
2014-08-06 11:52:10 -0400

    Adding in limit to number of characters in a node, changing default font and size

--- creategraph.rb
+++ creategraph.rb
@@ -5,12 +5,16 @@ unless ARGV[0]
   puts "usage: ruby #{$0} <data file, pipe separated>"
   exit -1
 end
-nodenames = {};
+nodenames = {}
+
+node_character_limit = 30
+weight = 0
 digraph do
   
   # Set the font name and size.  Details here:  http://www.graphviz.org/doc/fontfaq.txt
-  the_font = font "Helvetica-Bold"
-  the_fontsize = fontsize "12"
+  the_font = font "Times"
+  #the_font = font "Helvetica"
+  the_fontsize = fontsize "10"
   # set box color
   the_color = color "blueviolet"
   node_attribs << the_fontsize << the_font << the_color
@@ -28,9 +32,22 @@ digraph do
     current =  line[0]
     base =  line[0].gsub(/\.[^.]+$/,"");
     base = base.to_s
-    nodenames[current] = "" + current+" "+line[1].to_s
+
+    # Contents of each node
+    nodenames[current] = "" + current+" "+line[1].strip
+
+    # Truncate
+    if nodenames[current].length > node_character_limit
+      #To only do for level 3+, count the dots like this:  if current.count(".") >= 2
+      nodenames[current] = nodenames[current][0..node_character_limit] + "..."
+    end
     if current != base
-      edge nodenames[base], nodenames[current]
+      curr_edge = edge nodenames[base], nodenames[current]
+      #Tried messing with line weight (aka, length) but it isn't working
+      #if (current.count(".") == 2) 
+      #  weight = weight +1;
+      #  curr_edge.attributes << "weight=#{(weight % 3) * 5+5}."
+      #end
     end
   end
 
